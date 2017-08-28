12b82860ad4ca6fd8a5bef73868866899fe99dc6
Barbier <gabrielnasb@gmail.com>
2017-08-28 16:04:07 -0300

    Add Mock data and fetch API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,29 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'))
 })
 
+app.get('/users', (req, res) => {
+  res.json([
+    {
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "bob@mail.com",
+    },
+    {
+      "id": 2,
+      "firstName": "Tammy",
+      "lastName": "Norton",
+      "email": "tnorton@mail.com",
+    },
+    {
+      "id": 3,
+      "firstName": "Tina",
+      "lastName": "Lee",
+      "email": "lee.tina@mail.com",
+    },
+  ])
+})
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err)
